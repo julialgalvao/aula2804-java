f6458c75b981d114fb88b546a1e38ea7c9004b9b
julialgalvao <julialgalvao777@gmail.com>
2023-04-28 10:47:30 -0300

    aula de java

---
+++ script.js
@@ -0,0 +1,9 @@
+// DELARANDO A VARIAVEL MSG 
+let msg =document.getElementById('msg')
+
+// criar objeto com array
+const usuaria =[
+    {nome: "HUGUINHO", idade:18},
+    {nome: "LUIZINHO", idade:19},
+    {nome: "ZEZINHO", idade:20},
+];
